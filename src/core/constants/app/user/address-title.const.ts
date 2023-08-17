206933ffa40159637dadbe88d2234677f28d2e4a
lucifergotmad <amateur.fullstackdev@gmail.com>
2023-08-17 15:28:03 +0700

    feat:[minor] add const for address titile and type

---
+++ src/core/constants/app/user/address-title.const.ts
@@ -0,0 +1,5 @@
+export enum AddressTitle {
+  Office = "Kantor",
+  Home = "Rumah",
+  Other = "Lainnya",
+}
---
+++ src/core/constants/app/user/address-type.const.ts
@@ -0,0 +1,4 @@
+export enum AddressType {
+  City = "Kota",
+  Regency = "Kabupaten",
+}
