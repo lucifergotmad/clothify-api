2073ad1bc4f77e7d44247d5ac3a2ff750b2ceca2
lucifergotmad <amateur.fullstackdev@gmail.com>
2023-08-17 15:32:26 +0700

    feat:[major] add new value-objects title and type

---
+++ src/modules/member-address/domain/value-objects/title.value-object.ts
@@ -0,0 +1,20 @@
+import { UnprocessableEntityException } from "@nestjs/common";
+import { ValueObject } from "src/core/base-classes/domain/value-object";
+import { DomainPrimitive } from "src/core/base-classes/types/domain-primitive.type";
+import { Guard } from "src/core/logic/guard";
+
+export class Title extends ValueObject<string> {
+  constructor(value: string) {
+    super({ value });
+  }
+
+  get value() {
+    return this.props.value;
+  }
+
+  protected validate({ value }: DomainPrimitive<string>): void {
+    if (Guard.isInvalidAddressTitle(value)) {
+      throw new UnprocessableEntityException("Address Title not valid!");
+    }
+  }
+}
