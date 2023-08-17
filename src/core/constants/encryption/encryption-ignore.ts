c2215dea5025119278bca09baad6479e8f0ddbba
lucifergotmad <amateur.fullstackdev@gmail.com>
2023-08-17 15:26:00 +0700

    fix:[minor] add encryption ignore for member & address

--- src/core/constants/encryption/encryption-ignore.ts
+++ src/core/constants/encryption/encryption-ignore.ts
@@ -1,1 +1,3 @@
 export const UserIgnore = ["_id", "password", "level", "username"];
+export const MemberIgnore = ["_id", "member_id", "username", "password"];
+export const MemberAddressIgnore = ["_id", "member_id"];
